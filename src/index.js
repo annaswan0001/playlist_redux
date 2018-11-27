1dd3fbc837e1a3e542ab1e5e4b2adbafc11a891f
Anna Samoylenko <asamoylenko@ioc.gov.ua>
2018-11-27 15:11:07 +0200

    create store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState=[]
+
+function playlist (state=initialState, action){
+    switch(action.type){
+        case"ADD_TRACK":{
+            return[...state, action.payload]
+        }
+        default:{
+return state
+        }
+    }
+
+}
+const store = createStore(playlist)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+
+, document.getElementById('root'));
+
+// const addTrackbtn = document.querySelectorAll('.addTrack')[0];
+// const trackValue = document.querySelectorAll('.trackInput')[0];
+// const list = document.querySelectorAll('.list')[0];
+
+// 
+// store.subscribe(
+//     ()=>{
+//         console.log(store.getState())
+//         list.innerHTML="";
+//         trackValue.value =""
+//         store.getState().map(track => {
+//             const li = document.createElement("li");
+//             li.textContent=track;
+//             list.appendChild(li)
+//         }
+            
+            
+//     )
+//     }
+    
+
+// )
+
+
+
+
+
+// addTrackbtn.addEventListener('click',
+// function(){
+//     const inputValue = trackValue.value
+//     store.dispatch({type:"ADD_TRACK",payload:inputValue})
+// })
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
